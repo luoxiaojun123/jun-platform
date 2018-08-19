4b8b1d60abc9126f85f29518a99351eb3288c6ce
luoxiaojun123 <lxjluoxiaojun@163.com>
2018-08-19 22:07:34 +0800

    Activiti 工作流相关代码提交

---
+++ base-services/src/main/java/xiaojun/config/ActivitiConfig.java
@@ -0,0 +1,84 @@
+package xiaojun.config;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import org.activiti.engine.*;
+import org.activiti.spring.SpringProcessEngineConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+
+/**
+ * @author xiaojun
+ * @date 2018/8/19 15:11
+ */
+@Configuration
+public class ActivitiConfig {
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public ProcessEngine processEngine() throws IOException {
+
+        SpringProcessEngineConfiguration configuration = new SpringProcessEngineConfiguration();
+
+        ///Resource[] resources = new PathMatchingResourcePatternResolver().getResources(ResourceLoader.CLASSPATH_URL_PREFIX + "/processes/*.bpmn");
+
+        configuration.setTransactionManager(dataSourceTransactionManager());
+        configuration.setDataSource(dataSource());
+        configuration.setDatabaseSchemaUpdate("true");
+        ///configuration.setDeploymentResources(resources);
+        configuration.setDbIdentityUsed(false);
+        return configuration.buildProcessEngine();
+    }
+
+    @Bean
+    public RepositoryService repositoryService(ProcessEngine processEngine) {
+        return processEngine.getRepositoryService();
+    }
+
+    @Bean
+    public RuntimeService runtimeService(ProcessEngine processEngine) {
+        return processEngine.getRuntimeService();
+    }
+
+    @Bean
+    public TaskService taskService(ProcessEngine processEngine) {
+        return processEngine.getTaskService();
+    }
+
+    @Bean
+    public HistoryService historyService(ProcessEngine processEngine) {
+        return processEngine.getHistoryService();
+    }
+
+    @Bean
+    public ManagementService managementService(ProcessEngine processEngine) {
+        return processEngine.getManagementService();
+    }
+
+    @Bean
+    public IdentityService identityService(ProcessEngine processEngine) {
+        return processEngine.getIdentityService();
+    }
+
+
+    @Bean
+    public DataSourceTransactionManager dataSourceTransactionManager(){
+        return new DataSourceTransactionManager(dataSource());
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DruidDataSource dataSource = new DruidDataSource();
+        dataSource.setUrl(env.getProperty("spring.datasource.url"));
+        dataSource.setUsername(env.getProperty("spring.datasource.username"));
+        dataSource.setPassword(env.getProperty("spring.datasource.password"));
+        return dataSource;
+    }
+}
---
+++ base-services/src/main/java/xiaojun/listener/activiti/MyExcutionListener.java
@@ -0,0 +1,20 @@
+package xiaojun.listener.activiti;
+
+import lombok.extern.slf4j.Slf4j;
+import org.activiti.engine.delegate.DelegateExecution;
+import org.activiti.engine.delegate.ExecutionListener;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author xiaojun
+ * @date 2018/8/19 21:40
+ */
+@Component
+@Slf4j
+public class MyExcutionListener implements ExecutionListener {
+
+    @Override
+    public void notify(DelegateExecution execution) {
+        log.info("Activiti自定义的监听器执行了");
+    }
+}
