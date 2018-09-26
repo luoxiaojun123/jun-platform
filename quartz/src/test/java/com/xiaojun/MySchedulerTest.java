7741dc383e6523d5748969264c9c8e5ef0344716
long.luo <1049442642@qq.com>
2018-09-26 15:19:19 +0800

    quartz相关代码提交

--- quartz/src/test/java/com/xiaojun/MySchedulerTest.java
+++ quartz/src/test/java/com/xiaojun/MySchedulerTest.java
@@ -4,7 +4,6 @@ import com.xiaojun.scheduler.MyScheduler;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.omg.CORBA.PUBLIC_MEMBER;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
