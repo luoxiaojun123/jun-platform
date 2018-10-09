d37842743618cf2f54d61373944298c8384002dc
long.luo <1049442642@qq.com>
2018-10-09 16:01:17 +0800

    注解实现 redis分布式锁

--- redis/src/main/java/com/xiaojun/aop/RedisAop.java
+++ redis/src/main/java/com/xiaojun/aop/RedisAop.java
@@ -9,7 +9,6 @@ import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.expression.spel.support.StandardEvaluationContext;
