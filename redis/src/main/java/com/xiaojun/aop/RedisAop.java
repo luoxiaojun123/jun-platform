6a49951520567c3828c7c1e30740633168cd23cf
long.luo <1049442642@qq.com>
2018-10-10 17:30:53 +0800

    注解实现 redis分布式锁

--- redis/src/main/java/com/xiaojun/aop/RedisAop.java
+++ redis/src/main/java/com/xiaojun/aop/RedisAop.java
@@ -8,6 +8,7 @@ import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.expression.MethodBasedEvaluationContext;
 import org.springframework.core.LocalVariableTableParameterNameDiscoverer;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
@@ -109,6 +110,32 @@ public class RedisAop {
             context.setVariable(paraNameArr[i], args[i]);
         }
         return parser.parseExpression(key).getValue(context, String.class);
+    }
+
+
+    /**
+     * 支持 #p0 参数索引的表达式解析
+     *
+     * @param rootObject 根对象,method 所在的对象
+     * @param spel       表达式
+     * @param method     ，目标方法
+     * @param args       方法入参
+     * @return 解析后的字符串
+     */
+    private String parseKey(Object rootObject, String spel, Method method, Object[] args) {
+        //获取被拦截方法参数名列表(使用Spring支持类库)
+        LocalVariableTableParameterNameDiscoverer u =
+                new LocalVariableTableParameterNameDiscoverer();
+        String[] paraNameArr = u.getParameterNames(method);
+        //使用SPEL进行key的解析
+        ExpressionParser parser = new SpelExpressionParser();
+        //SPEL上下文
+        StandardEvaluationContext context = new MethodBasedEvaluationContext(rootObject, method, args, u);
+        //把方法参数放入SPEL上下文中
+        for (int i = 0; i < paraNameArr.length; i++) {
+            context.setVariable(paraNameArr[i], args[i]);
+        }
+        return parser.parseExpression(spel).getValue(context, String.class);
     }
 
 }
