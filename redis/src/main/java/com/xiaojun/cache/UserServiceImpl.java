ca8a4ad7348082d6df8d3bde7474d19c01fb84e1
long.luo <1049442642@qq.com>
2018-12-12 16:01:49 +0800

    test

--- redis/src/main/java/com/xiaojun/cache/UserServiceImpl.java
+++ redis/src/main/java/com/xiaojun/cache/UserServiceImpl.java
@@ -59,4 +59,7 @@ public class UserServiceImpl implements UserService {
             e.printStackTrace();
         }
     }
+    public static void main(String[] args){
+        System.out.println("我要测试");
+    }
 }
