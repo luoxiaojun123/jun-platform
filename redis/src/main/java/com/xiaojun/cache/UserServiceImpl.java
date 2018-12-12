a83bc8c760938a9b779c14f239c5e737aa71b402
long.luo <1049442642@qq.com>
2018-12-12 16:05:04 +0800

    回退版本

--- redis/src/main/java/com/xiaojun/cache/UserServiceImpl.java
+++ redis/src/main/java/com/xiaojun/cache/UserServiceImpl.java
@@ -59,7 +59,4 @@ public class UserServiceImpl implements UserService {
             e.printStackTrace();
         }
     }
-    public static void main(String[] args){
-        System.out.println("我要测试");
-    }
 }
