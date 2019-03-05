6ef9547cdc8802863eff48e11ec702436c5bb0bf
long.luo <1049442642@qq.com>
2019-03-05 18:41:06 +0800

    修改接口

--- zuul-swagger-oauth2.0/zuul/src/main/java/com/xiaojun/controller/ServiceController.java
+++ zuul-swagger-oauth2.0/zuul/src/main/java/com/xiaojun/controller/ServiceController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ServiceController {
 
 
-    @GetMapping("/zull")
+    @GetMapping("/test_zull")
     public String test(){
         return "zull success";
     }
